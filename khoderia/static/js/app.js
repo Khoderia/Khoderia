25834ea0df887bb8b20b7d7bfbce5590da4380e7
ramazanemreosmanoglu <ramazanemreosmanoglu@gmail.com>
2020-12-19 20:48:21 +0300

    Arrange front end

---
+++ khoderia/static/js/app.js
@@ -0,0 +1,9 @@
+// Khoderia App
+// Ramazan Emre Osmanoglu
+
+function search_on_duck(url) {
+  // Does a search on duck duck go
+}
+
+// TODO: Add current page css style to navbar according to current page
+// <span class="sr-only">(current)</span>
